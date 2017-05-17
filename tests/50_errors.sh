19637382f89c8a3139faafe017e3166c9c36696d
Alexandre Pujol <alexandre@pujol.io>
2017-05-17 23:18:23 +0100

    Test: Add public only gpg key & related test

--- tests/50_errors.sh
+++ tests/50_errors.sh
@@ -10,6 +10,10 @@ test_expect_success 'Password tomb creation with invalid key' '
     test_must_fail _pass tomb $KEY_INVALID --debug --unsafe
     '
 
+test_expect_success 'Testing store creation with a public key' '
+    test_must_fail _pass tomb $KEY_PUBLIC --verbose --unsafe
+    '
+
 test_expect_success 'Testing wrong tomb parameters' '
     PASSWORD_STORE_TOMB_SIZE=5 test_must_fail _pass tomb $KEY1 --quiet --unsafe &&
     PASSWORD_STORE_TOMB_FILE="$TMP/password.tomb" test_must_fail _pass tomb $KEY1 --quiet --unsafe &&
