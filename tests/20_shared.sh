ed303679a4a6419d03a3c908326bb1a335635f03
Alexandre Pujol <alexandre@pujol.io>
2018-12-18 14:09:10 +0000

    Cosmetic

--- tests/20_shared.sh
+++ tests/20_shared.sh
@@ -5,7 +5,7 @@ export test_description="pass-tomb with shared keys."
 cd tests
 source ./commons
 
-test_export "shared"
+test_export shared
 test_expect_success 'Testing a shared password tomb' '
     _pass tomb $KEY1 $KEY2 $KEY3 --verbose --unsafe &&
     _pass_populate &&
