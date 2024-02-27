453032ac9e808664a66bbadaea5398d15633e469
Alexandre Pujol <alexandre@pujol.io>
2024-02-27 19:16:38 +0000

    feat: increase default tomb size to 30MB.
    Co-authored-by: @alexxroche

--- tomb.bash
+++ tomb.bash
@@ -21,7 +21,7 @@
 readonly TOMB="${PASSWORD_STORE_TOMB:-tomb}"
 readonly TOMB_FILE="${PASSWORD_STORE_TOMB_FILE:-$HOME/.password.tomb}"
 readonly TOMB_KEY="${PASSWORD_STORE_TOMB_KEY:-$HOME/.password.tomb.key}"
-readonly TOMB_SIZE="${PASSWORD_STORE_TOMB_SIZE:-10}"
+readonly TOMB_SIZE="${PASSWORD_STORE_TOMB_SIZE:-30}"
 
 readonly VERSION="1.3"
 
